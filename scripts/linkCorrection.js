16ba670d95c64e62a7f8d02e223661b89a7677d6
SylvainJanet <sylvainjanet@wanadoo.fr>
2023-02-17 13:15:09 +0100

    sonarcloud corrections

--- scripts/linkCorrection.js
+++ scripts/linkCorrection.js
@@ -1,6 +1,6 @@
 import { pathPrefix } from "./pathPrefix.js";
 
 let items = document.getElementsByClassName("linkToFix");
-for (var i = 0; i < items.length; i++) {
-  items[i].setAttribute("href", pathPrefix + items[i].getAttribute("href"));
+for (item of items) {
+  item.setAttribute("href", pathPrefix + item.getAttribute("href"));
 }
